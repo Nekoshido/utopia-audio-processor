1dd50246ae03ae3e699357721915db61ea1ba7b8
Nekoshido <hordonvi@gmail.com>
2022-01-06 10:13:14 +0100

    Update changes, file storage and testing

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as requirements:
+    dependencies = requirements.read().strip().split("\n")
+
+package = "utopia-audio-processor"
+version = "1.0.0"
+
 setup(
-    name='Utopia Audio Processor',
-    version='1.0',
+    name=package,
+    version=version,
+    install_requires=dependencies,
     packages=find_packages()
 )
--- test/test_extractor.py
+++ test/test_extractor.py
@@ -1,17 +1,25 @@
-from setuptools import find_packages
+from numpy import array, ndarray
 
 from src.extractor import Extractor
 from unittest import TestCase
 
 
-class ExctractorTest(TestCase):
-    extractor = Extractor()
+class ExtractorTest(TestCase):
+    extractor = Extractor("test.yaml")
 
-    def test_normalize_unicode_to_ascii(self):
-        input_string = "Mickæël"
-        expected = 'mickel'
+    @staticmethod
+    def _sample_numpy() -> ndarray:
+        return array([1, 2, 3, 4, 5])
 
-        real = self.file_processer._normalize_unicode_to_ascii(input_string)
-        assert real == expected
+    def _extractor_result_template(self):
+
+    def test_feature_extractor(self):
+        audio_array = self._sample_numpy()
+        real = self.extractor.feature_extractor(audio_array, 5)
+        # assert real == expected
 
 
+extractor = Extractor("test.yaml")
+audio_array = array([1.0, 2.0, 3.0])
+real = extractor.feature_extractor(audio_array, 2)
+print(real)
